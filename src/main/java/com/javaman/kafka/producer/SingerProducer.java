e09dc74439169e6ea383395eb2b85b17f0aec7af
pengzhe <pengzhe@rayvision.com>
2017-12-16 12:35:34 +0800

    init

--- src/main/java/com/javaman/kafka/producer/SingerProducer.java
+++ src/main/java/com/javaman/kafka/producer/SingerProducer.java
@@ -1,10 +1,10 @@
 package com.javaman.kafka.producer;
 
 import com.javaman.kafka.bean.StockQuotationInfo;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.text.DecimalFormat;
 import java.util.Properties;
 import java.util.Random;
 
@@ -54,10 +54,59 @@ public class SingerProducer {
     private static StockQuotationInfo createQuotationInfo() {
         StockQuotationInfo quotationInfo = new StockQuotationInfo();
         //随机产生1到10的整数,然后与600100相加组成股票代码
-        Random random = new Random();
-        Integer stockCode = random.nextInt(10) + 60010;
+        Random r = new Random();
+        Integer stockCode = r.nextInt(10) + 60010;
+        //产生一个0~1之间的随机浮点数
+        float random = (float) Math.random();
+        //设置涨跌规则
+        if (random / 2 < 0.5) {
+            random = -random;
+        }
+        //保存两位有效数字
+        DecimalFormat decimalFormat = new DecimalFormat(".00");
+        //设置最新价在11元浮动
+        quotationInfo.setCurrentPrice(Float.valueOf(decimalFormat.format(11 + random)));
+        //设置昨日收盘价为固定值
+        quotationInfo.setPreClosePrice(11.80f);
+        //设置开盘价
+        quotationInfo.setOpenPrce(11.5f);
+        //设置最低价,并不考虑10%限制,以及当前价是否已是最低价
+        quotationInfo.setLowPrice(10.5f);
+        //设置最高价,并不考虑10%限制,以及当前价是否已是最高价
+        quotationInfo.setHighPrice(12.5f);
+        quotationInfo.setStockCode(stockCode.toString());
+        quotationInfo.setStockName("股票--" + stockCode);
         return quotationInfo;
     }
 
+    public static void main(String[] args) {
+        ProducerRecord<String, String> record = null;
+        StockQuotationInfo quotationInfo = null;
+        try {
+            int num = 0;
+            for (int i = 0; i < MSG_SIZE; i++) {
+                quotationInfo = createQuotationInfo();
+                record = new ProducerRecord<String, String>(TOPIC, null,
+                        quotationInfo.getTradeTime(), quotationInfo.getStockCode(),
+                        quotationInfo.toString());
+                //异步发送消息
+                producer.send(record, new Callback() {
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        if (exception != null) {
+                            //发送异常记录异常信息
+                            System.out.println("发送消息回调异常:Send message occurs exception," + exception);
+                        }
+                        if (metadata != null) {
+                            System.out.println("offset" + metadata.offset());
+                        }
 
+                    }
+                });
+            }
+        } catch (Exception e) {
+            System.out.println("生产者异常:Send message occurs exception," + e);
+        } finally {
+            producer.close();
+        }
+    }
 }
